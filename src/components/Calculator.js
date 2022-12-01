678974ddc72ab0e785d579f9c311b0d12c209756
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-12-01 01:00:54 +0100

    feat: Add events to buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,31 +2,48 @@ import React from 'react';
 import CalculatorButton from './CalculatorButton';
 import CalculatorInput from './CalculatorInput';
 import styles from './Calculator.module.css';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const element = calculate(this.state, event.target.innerHTML);
+    this.setState(element);
+  }
+
   render() {
+    const { total, operation, next } = this.state;
     return (
       <div id="calculator" className={styles.calculator}>
-        <CalculatorInput />
-        <CalculatorButton classn="btnAC " value="AC" />
-        <CalculatorButton classn="btnSigns " value="+/-" />
-        <CalculatorButton classn="btn% " value="%" />
-        <CalculatorButton classn="btn/ " value="÷" />
-        <CalculatorButton classn="btn7 " value="7" />
-        <CalculatorButton classn="btn8 " value="8" />
-        <CalculatorButton classn="btn9 " value="9" />
-        <CalculatorButton classn="btn* " value="x" />
-        <CalculatorButton classn="btn4 " value="4" />
-        <CalculatorButton classn="btn5 " value="5" />
-        <CalculatorButton classn="btn6 " value="6" />
-        <CalculatorButton classn="btn- " value="-" />
-        <CalculatorButton classn="btn1 " value="1" />
-        <CalculatorButton classn="btn2 " value="2" />
-        <CalculatorButton classn="btn3 " value="3" />
-        <CalculatorButton classn="btn+ " value="+" />
-        <CalculatorButton classn="btn0 " value="0" />
-        <CalculatorButton classn="btn. " value="." />
-        <CalculatorButton classn="btn= " value="=" />
+        <CalculatorInput value={`${total} ${next} ${operation}`} />
+        <CalculatorButton onClick={this.handleClick} classn="btnAC " value="AC" />
+        <CalculatorButton onClick={this.handleClick} classn="btnSigns " value="+/-" />
+        <CalculatorButton onClick={this.handleClick} classn="btn% " value="%" />
+        <CalculatorButton onClick={this.handleClick} classn="btn/ " value="÷" />
+        <CalculatorButton onClick={this.handleClick} classn="btn7 " value="7" />
+        <CalculatorButton onClick={this.handleClick} classn="btn8 " value="8" />
+        <CalculatorButton onClick={this.handleClick} classn="btn9 " value="9" />
+        <CalculatorButton onClick={this.handleClick} classn="btn* " value="x" />
+        <CalculatorButton onClick={this.handleClick} classn="btn4 " value="4" />
+        <CalculatorButton onClick={this.handleClick} classn="btn5 " value="5" />
+        <CalculatorButton onClick={this.handleClick} classn="btn6 " value="6" />
+        <CalculatorButton onClick={this.handleClick} classn="btn- " value="-" />
+        <CalculatorButton onClick={this.handleClick} classn="btn1 " value="1" />
+        <CalculatorButton onClick={this.handleClick} classn="btn2 " value="2" />
+        <CalculatorButton onClick={this.handleClick} classn="btn3 " value="3" />
+        <CalculatorButton onClick={this.handleClick} classn="btn+ " value="+" />
+        <CalculatorButton onClick={this.handleClick} classn="btn0 " value="0" />
+        <CalculatorButton onClick={this.handleClick} classn="btn. " value="." />
+        <CalculatorButton onClick={this.handleClick} classn="btn= " value="=" />
 
       </div>
     );
