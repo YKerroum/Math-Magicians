d4f70418bbd02a7a11509d7a0cc30c9a651e280f
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-12-08 02:20:21 +0100

    feat: implement routers and create new pages

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import { LoremIpsum } from 'react-lorem-ipsum';
+
+const Home = () => (
+  <main>
+    <h1>Welcome to our page!</h1>
+    <LoremIpsum p={2} avgSentencesPerParagraph={14} />
+  </main>
+);
+
+export default Home;
